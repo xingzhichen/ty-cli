ebe2b105ca55b38e0b1cad0d12847fda0796e8d5
xingzhichen <x2822074239@gmail.com>
2018-12-24 23:08:50 +0800

    快速启动

---
+++ README.md
@@ -0,0 +1,11 @@
+# typora-cli
+
+quick open Typora
+
+## use
+
+```shell
+
+ty ./README.md
+ty ./dir1/dir2/README.md  // Automatically created when a file or folder does not exist
+```
---
+++ index.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+const argv = require("yargs").argv;
+const shell = require("shelljs");
+const path = require("path");
+const fs = require("fs-extra");
+const { _: args } = argv;
+const type = "md";
+const tyBaseShell = "open -a typora";
+const getPath = name =>
+  name ? path.resolve(process.cwd(), name) : process.cwd();
+let filePath = getPath(args[0]);
+if (!fs.pathExistsSync(filePath)) {
+  path.extname(filePath)
+    ? fs.ensureFileSync(filePath)
+    : fs.ensureDirSync(filePath);
+}
+shell.exec(`${tyBaseShell} ${filePath}`);
