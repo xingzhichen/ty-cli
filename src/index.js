5053f810aa16361c271e26bc43be1dc310831ed3
xingzhichen <x2822074239@gmail.com>
2018-12-26 22:29:55 +0800

    适配window

--- src/index.js
+++ src/index.js
@@ -5,9 +5,13 @@ const path = require("path");
 const fs = require("fs-extra");
 const { _: args } = argv;
 const type = "md";
-const tyBaseShell = "open -a typora";
+const os = require("os");
 const getPath = name =>
   name ? path.resolve(process.cwd(), name) : process.cwd();
+const tyBaseShell = os.platform().match(/^win/)
+  ? "start typora"
+  : "open -a typora";
+
 let filePath = getPath(args[0]);
 if (!fs.pathExistsSync(filePath)) {
   path.extname(filePath)
